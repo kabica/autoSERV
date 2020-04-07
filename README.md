2c949dd8b4668d90929b0f457436c90b49e50801
Kevin Alexander <kalex.bica@gmail.com>
2020-04-07 16:13:48 -0600

    setup API server and inital routes using express

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## autoSERV
 API serving start-up code for all things Web Developer 
+
+### SSH-Agent 
+ssh-add ~/.ssh/id_rsa 
---
+++ src/index.js
@@ -0,0 +1,23 @@
+// load ENV data into process.ENV
+require('dotenv').config();
+
+// API server config
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
+
+const chalk = require('chalk');
+const morgan     = require('morgan');
+const express    = require("express");
+const bodyParser = require("body-parser");
+const apiRoutes = require('../apiRoutes');
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/', apiRoutes());
+
+app.listen(PORT, () => {
+  console.log(chalk.magenta('autoSERV'),`ascension initalization complete, now listening on PORT: ${PORT}`);
+})
